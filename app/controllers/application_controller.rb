e0e7f8b55b41ce7bd83fa6a2779b342e74ea4d2e
marianar97 <Ramirez@MacBook-Pro-de-Mariana.local>
2017-10-18 21:10:17 -0500

    Corrigiendo error de redirección y poniendo vista show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :parametros_permitidos_modifica, if: :devise_controller?
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
     
     protected
 
