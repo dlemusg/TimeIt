e39823245bb016b5759368323d300e7b558983d1
carlos18bp <carlos18bp@gmail.com>
2017-10-22 18:00:31 -0500

    Corregido error notificaciones

--- app/controllers/frame_controller.rb
+++ app/controllers/frame_controller.rb
@@ -2,39 +2,40 @@ class FrameController < ApplicationController
   before_action :authenticate_user!
   def agregarOferta
     @offer = Offer.new
-  end
-
-  def categorias
-    @categories = Category.all
-  end
-
-  def estadoSolicitud
-  end
-
-  def historial
-  end
-
-  def inicio
-  end
-
-  def perfilPrivado
-  end
-
-  def perfilPublico
-  end
-
-  def solicitudesPend
-  end
-
-  def verDetallesOferta
-  end
-
-  def _pestanasPrincipales
     @ver_oferta_todas = Offer.all   
     @request = Request.new       
     @userRequest = Request.where(:user_id => current_user.id)
   end
 
+  def categorias
+    @categories = Category.all
+    @offer = Offer.new
+    @ver_oferta_todas = Offer.all   
+    @request = Request.new       
+    @userRequest = Request.where(:user_id => current_user.id)
+  end 
+
+  def historial
+    @offer = Offer.new
+    @ver_oferta_todas = Offer.all   
+    @request = Request.new       
+    @userRequest = Request.where(:user_id => current_user.id)
+  end
+
+  def inicio
+    @offer = Offer.new
+    @ver_oferta_todas = Offer.all   
+    @request = Request.new       
+    @userRequest = Request.where(:user_id => current_user.id)
+  end
+
+  def verDetallesOferta
+    @offer = Offer.new
+    @ver_oferta_todas = Offer.all   
+    @request = Request.new       
+    @userRequest = Request.where(:user_id => current_user.id)
+  end
+ 
   def verOfertas       
      @cate = params[:cate]     
      @ver_oferta_categoria = Offer.where(:category_id =>@cate)
