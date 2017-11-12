1bb5f5296e01ac534a574d0f0d77aa50aefdb89d
camilor <jrodri92@eafit.edu.co>
2017-11-11 23:03:13 -0500

    ya es posible decir cuanto se pagará y agregar comentarios, falta congelar el tiempo

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,7 +1,5 @@
 class ContractsController < ApplicationController
 
-
-
   def new
     @contract = Contract.new
   end
@@ -9,12 +7,24 @@ class ContractsController < ApplicationController
   def show
     @contract = Contract.find_by_id(params[:id])
   end
+
+  def update
+    @contract = Contract.find_by_id(params[:id])
+    respond_to do |format|
+      if @contract.update(contract_params)
+        format.html { redirect_to @contract}
+        format.json { render :show, status: :ok, location: @contract }
+      else
+        format.json { render json: @offer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   
   def destroy
     @contract = Contract.find_by_id(params[:id])
     @contract.destroy
     respond_to do |format|
-      format.html { redirect_to frame_inicio_path, notice: 'Offer was successfully destroyed.' }
+      format.html { redirect_to frame_inicio_path }
       format.json { head :no_content }
     end
   end
@@ -23,7 +33,7 @@ class ContractsController < ApplicationController
     @contract = Contract.new(contract_params)
     respond_to  do |format|
       if @contract.save
-        format.html { redirect_to @contract, notice: 'Offer was successfully created.' }
+        format.html { redirect_to @contract }
         format.json { render :show, status: :created, location: @contract}
       else
         format.json { render json: @contract.errors, status: :unprocessable_entity }
